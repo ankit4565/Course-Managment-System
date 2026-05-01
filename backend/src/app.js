3e95b4135c36a67b41afedbafcc6e93ef4de2714
ankit4565 <ankitbowade3731@gmail.com>
2026-05-02 01:44:20 +0530

    frontend connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
